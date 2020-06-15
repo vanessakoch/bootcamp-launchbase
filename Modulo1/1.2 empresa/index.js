f501fdb6c623a49c0934f1d40a3e8ce3b7d51274
vanessakoch <vanessa.r15@aluno.ifsc.edu.br>
2020-06-15 16:58:50 -0300

    incluindo 1.2 empresa

---
+++ Modulo1/1.2 empresa/index.js
@@ -0,0 +1,11 @@
+const empresa = {
+  nome: 'Rocketseat',
+  cor: 'Roxo',
+  foco: 'Programação',
+  endereco: {
+    rua: 'Guilherme Gembala',
+    numero: 260,
+  }
+}
+
+console.log(`A empresa ${empresa.nome} está localizada em Rua ${empresa.endereco.rua}, ${empresa.endereco.numero}`);
