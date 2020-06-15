e8ed681a25732995be1f65f754f6f3fa4972897b
vanessakoch <vanessa.r15@aluno.ifsc.edu.br>
2020-06-15 18:05:15 -0300

    importando desafio 1.3 tecnologias

---
+++ Modulo1/1.3tecnologias/index.js
@@ -0,0 +1,29 @@
+// Usuários e tecnologias
+
+const usuarios = [
+  { nome: "Vanessa", tecnologias: ["HTML", "CSS", "JavaScript"] },
+  { nome: "Felipe", tecnologias: ["JavaScript", "CSS"] },
+  { nome: "Larissa", tecnologias: ["Node.js"] }
+];
+
+for (let i = 0; i < usuarios.length; i++) {
+  console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`)
+}
+
+// Busca por tecnologia
+
+function checarCss(usuario) {
+  for (let tecnologia of usuario.tecnologias) {
+      if (tecnologia === 'CSS') 
+        return true
+  }
+  return false
+}
+
+for (let i = 0; i < usuarios.length; i++) {
+  const trabalhaComCss = checarCss(usuarios[i])
+
+  if(trabalhaComCss) {
+    console.log(`O usuário ${usuarios[i].nome} trabalha com CSS.`)
+  }
+}
