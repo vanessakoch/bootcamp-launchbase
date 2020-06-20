0d6cbe55860628c90fbb3e7c5c9777dc124c0f3d
vanessakoch <vanessa.r15@aluno.ifsc.edu.br>
2020-06-20 15:40:12 -0300

    frontend pronto

--- Modulo2/Foodfy/scripts.js
+++ Modulo2/Foodfy/scripts.js
@@ -4,10 +4,17 @@ const modal = document.querySelector('.modal')
 
 for(let card of cards) {
   card.addEventListener("click", function () {
+    const modalTitle = card.querySelector('.card-info').querySelector('h3').textContent;
+    const modalDescription = card.querySelector('.card-info').querySelector('p').textContent;
+
     modalOverlay.classList.add("active")
+    modalOverlay.querySelector("img").src = `/assets/${card.id}.png`
+    modalOverlay.querySelector("#modal-title").textContent = modalTitle
+    modalOverlay.querySelector("#modal-description").textContent = modalDescription
   })
 }
 
 document.querySelector(".modal-close").addEventListener("click", function () {
   modalOverlay.classList.remove("active")
+  //modalOverlay.querySelector("iframe").src = ""
 })
