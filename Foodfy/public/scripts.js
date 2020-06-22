c491bdb229fd78dcddcdcdebe96a17768363803c
vanessakoch <vanessa.r15@aluno.ifsc.edu.br>
2020-06-22 15:46:48 -0300

    nova página estilizada, nova rota funcionando, importação de erro 404

--- Foodfy/public/scripts.js
+++ Foodfy/public/scripts.js
@@ -1,8 +1,36 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card')
+const cards = document.querySelectorAll('.card');
+const ingredients = document.querySelector('#ingredient-content');
+const preparation = document.querySelector('#preparation-content');
+const information = document.querySelector('#information-content');
+const btnIngredients = document.querySelector('#ingredients-btn');
+const btnPreparation = document.querySelector('#preparation-btn');
+const btnInformation = document.querySelector('#information-btn');
 
 for(let card of cards) {
   card.addEventListener("click", function () {
     window.location.href = `/recipes/${card.id}`
   })
 }
+
+const hideAndShow = function(botao, container) {
+  if(botao.textContent === 'ESCONDER') {
+    botao.textContent = 'MOSTRAR'
+    container.classList.add("hide")
+  } else {
+    botao.textContent = 'ESCONDER'
+    container.classList.remove("hide")
+  }
+}
+
+btnIngredients.addEventListener("click", function () {
+  hideAndShow(btnIngredients, ingredients)
+})
+
+btnPreparation.addEventListener("click", function () {
+  hideAndShow(btnPreparation, preparation)
+})
+
+btnInformation.addEventListener("click", function () {
+  hideAndShow(btnInformation, information)
+})
+
