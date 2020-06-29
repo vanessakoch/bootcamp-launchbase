eaf7dd5c5ca9840255bc6e805a8cc720f3b01566
vanessakoch <vanessa.r15@aluno.ifsc.edu.br>
2020-06-28 21:16:21 -0300

    iniciando a página de formulário para adicionar professores

--- Modulo4/routes.js
+++ Modulo4/routes.js
@@ -2,7 +2,19 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function(req, res) {
-  return res.render('index')
+  return res.redirect('/teachers')
+})
+
+routes.get('/teachers', function(req, res) {
+  return res.render('teachers/index')
+})
+
+routes.get('/teachers/create', function(req, res) {
+  return res.render('teachers/create')
+})
+
+routes.post('/teachers', function(req,res) {
+  return res.send('recebido')
 })
 
 module.exports = routes
